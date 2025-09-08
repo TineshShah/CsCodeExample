69efd7528283c085accb96968754abb0489ed608
Tinesh N SHAH <tinesh.shah@gmail.com>
2025-09-08 10:32:47 +0200

    test2

--- CsCodeExample/Program.cs
+++ CsCodeExample/Program.cs
@@ -14,7 +14,7 @@ class Program
 
         // break point(F5, F10), refactor, search and replace, Go to def., Find all ref., (un)comment
 
-        #region MyRegion Test1
+      #region MyRegion Test2
         // your collapsible code here
         #endregion
 
